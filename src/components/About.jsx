711a136d7c385ba73634eaba838ff575093b1cc3
Jimmy O <jimmyojeda94@gmail.com>
2023-02-03 23:35:59 -0800

    changed default images and text for project items

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,8 +18,7 @@ const About = () => {
           </div>
           <div>
             <p>
-              I am a passionate lorem ipsum stuff
-
+              I am a highly motivated software developer with a passion for writing clean and efficient code. I have 3 years of experience developing fullstack and frontend focused applications and a proven track record of delivering high-quality, scalable solutions. My expertise in Javascript, HTML, CSS, Node.JS, and technologies like Git, Postman and MongoDB aswell as being comfortable in a remote agile evironment has enabled me to effectively design and implement complex systems. I am constantly learning and staying up-to-date with the latest advancements in the field to ensure I am always delivering the best possible outcomes. In my free time, I enjoy spending time with my wife and dogs, I also enjoy playing video games and customizing the game to my preferences through custom code, implementing addon packs, etc. I am excited to bring my skills and enthusiasm to a new challenge and contribute to the success of your team.
             </p>
           </div>
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import WorkImg from '../assets/workImg.jpeg'
-import realEstate from '../assets/realestate.jpg'
+import Pathfinder from '../assets/2E-Core.jpg'
+import JournalMe from '../assets/journalMe2.jpg'
+import FreeFitness from '../assets/freeFitness.jpg'
 
 const Work = () => {
   return (
@@ -11,13 +12,13 @@ const Work = () => {
           <p className='py-6'>Check out some of my recent work</p>
         </div>
         {/* cotainer */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          {/* grid item */}
-          <div style={{ backgroundImage: `url(${WorkImg})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        <div className='grid sm:grid-cols md:grid-cols-2 gap-4'>
+          {/* grid item 1*/}
+          <div style={{ backgroundImage: `url(${Pathfinder})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             {/* hover effects */}
             <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+              <span className='flex justify-center text-2xl font-bold text-white tracking-wider'>
+                Pathfinder Sheet
               </span>
               <div className='pt-8 text-center'>
                 <a href="/">
@@ -30,11 +31,11 @@ const Work = () => {
             </div>
           </div>
           {/* grid item 2 */}
-          <div style={{ backgroundImage: `url(${realEstate})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+          <div style={{ backgroundImage: `url(${JournalMe})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             {/* hover effects */}
             <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+              <span className='flex justify-center text-2xl font-bold text-white tracking-wider'>
+                Journal Me
               </span>
               <div className='pt-8 text-center'>
                 <a href="/">
@@ -46,12 +47,12 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* grid item */}
-          <div style={{ backgroundImage: `url(${WorkImg})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+          {/* grid item 3*/}
+          <div style={{ backgroundImage: `url(${FreeFitness})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
             {/* hover effects */}
             <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+              <span className='flex justify-center text-2xl font-bold text-white tracking-wider'>
+                Free Fitness
               </span>
               <div className='pt-8 text-center'>
                 <a href="/">
@@ -63,12 +64,14 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* grid item 2 */}
-          <div style={{ backgroundImage: `url(${realEstate})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+          {/* grid item 4 */}
+          {/*style={{ backgroundImage: `url(${FreeFitness})` }}*/}
+          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div comingSoon'>
             {/* hover effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+            {/* className='opacity-0 group-hover:opacity-100' */}
+            <div className=' group-hover:opacity-100'>
+              <span className='flex justify-center text-2xl font-bold text-white tracking-wider'>
+                Coming Soon!
               </span>
               <div className='pt-8 text-center'>
                 <a href="/">
@@ -80,40 +83,7 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* grid item */}
-          <div style={{ backgroundImage: `url(${WorkImg})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            {/* hover effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* grid item 2 */}
-          <div style={{ backgroundImage: `url(${realEstate})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            {/* hover effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href="/">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
+
         </div>
       </div>
     </div>
